791bcd6b520e7c0f394c87a0e43158426850cce5
burlinchen <chenbeiliuhuo@gmail.com>
2024-02-22 17:07:51 +0800

    feat: next-greater-element-i

---
+++ cn/TypeScript/2567.修改两个元素的最小分数.ts
@@ -0,0 +1,7 @@
+// @algorithm @lc id=2706 lang=typescript 
+// @title minimum-score-by-changing-two-elements
+// @test([1,4,3])=0
+// @test([1,4,7,8,5])=3
+function minimizeSum(nums: number[]): number {
+
+};
