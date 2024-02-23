2586d727c75abc78b7e2bf35ad4c632f0f96d1fa
burlinchen <chenbeiliuhuo@gmail.com>
2024-02-23 11:33:42 +0800

    feat: minimum-score-by-changing-two-elements

--- cn/TypeScript/2567.修改两个元素的最小分数.ts
+++ cn/TypeScript/2567.修改两个元素的最小分数.ts
@@ -3,5 +3,8 @@
 // @test([1,4,3])=0
 // @test([1,4,7,8,5])=3
 function minimizeSum(nums: number[]): number {
-
-};
+    const sorted = nums.sort((a, b) => a - b);
+    
+    const n = sorted.length
+    return Math.min(sorted[n - 1] - sorted[2], sorted[n - 3] - sorted[0], sorted[n - 2] - sorted[1])
+  };
