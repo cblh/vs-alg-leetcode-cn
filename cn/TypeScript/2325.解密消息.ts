95caed4da154c287a58c078e6dacc5f1480fefff
burlinchen <chenbeiliuhuo@gmail.com>
2024-03-07 09:28:27 +0800

    feat: implement-queue-using-stacks

---
+++ cn/TypeScript/2325.解密消息.ts
@@ -0,0 +1,7 @@
+// @algorithm @lc id=2406 lang=typescript 
+// @title decode-the-message
+// @test("the quick brown fox jumps over the lazy dog","vkbs bs t suepuv")="this is a secret"
+// @test("eljuxhpwnyrdgtqkviszcfmabo","zwx hnfx lqantp mnoeius ycgk vcnjrdb")="the five boxing wizards jump quickly"
+function decodeMessage(key: string, message: string): string {
+    
+};
