a32736b6b6cc8889fc45abe1f9a198aa22305545
burlinchen <chenbeiliuhuo@gmail.com>
2024-03-27 15:08:40 +0800

    feat: minimum-moves-to-capture-the-queen

--- cn/TypeScript/3001.捕获黑皇后需要的最少移动次数.ts
+++ cn/TypeScript/3001.捕获黑皇后需要的最少移动次数.ts
@@ -12,32 +12,27 @@ function minMovesToCaptureTheQueen(
     e: number,
     f: number,
 ): number {
-    const dirs: number[][] = [
+    const dirs = [
         [-1, 0, 1, 0, -1],
-        [-1, 1, 1, -1, -1],
-    ];
+        [-1, 1, 1, -1, -1]
+    ]
     const check = (i: number, sx: number, sy: number, bx: number, by: number): boolean => {
-        for (let d = 0; d < 4; ++d) {
-            for (let k = 1; k < 8; ++k) {
-                const x = sx + dirs[i][d] * k;
-                const y = sy + dirs[i][d + 1] * k;
+        for (let d = 0; d < 4; d++) {
+            for (let k = 1; k < 8; k++) {
+                const x = sx + dirs[i][d] * k
+                const y = sy + dirs[i][d + 1] * k
                 if (x < 1 || x > 8 || y < 1 || y > 8) {
-                    break;
+                    break
                 }
                 if (x === bx && y === by) {
-                    break;
+                    break
                 }
                 if (x === e && y === f) {
-                    return true;
+                    return true
                 }
             }
         }
-        return false;
-    };
-    return check(0, a, b, c, d) || check(1, c, d, a, b) ? 1 : 2;
+        return false
+    }
+    return check(0, a, b, c, d) || check(1, c, d, a, b) ? 1 : 2
 }
-function minMovesToCaptureTheQueen2(a: number, b: number, c: number, d: number, e: number, f: number): number {
-    let ans = (a === e ? 0 : 1) + (b === f ? 0 : 1) + (c === e ? 1 : 0) + (d === f ? 1 : 0)
-    ans = Math.min(ans, (Math.abs(c - e) === Math.abs(d - f) ? 1 : 2) + (Math.abs(c - a) === Math.abs(d - b) ? 1 : 0))
-    return ans
-};
