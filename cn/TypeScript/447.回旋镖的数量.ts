de5b91b07b9e9eddb179dad0b2c5991da5190202
burlinchen <chenbeiliuhuo@gmail.com>
2024-04-05 10:34:45 +0800

    feat: number-of-boomerangs

--- cn/TypeScript/447.回旋镖的数量.ts
+++ cn/TypeScript/447.回旋镖的数量.ts
@@ -9,9 +9,11 @@ function numberOfBoomerangs(points: number[][]): number {
         const cnt = new Map()
         for (const [x2, y2] of points) {
             const d = (x1 - x2) ** 2 + (y1 - y2) ** 2
-            ans += cnt.get(d) ?? 0
             cnt.set(d, (cnt.get(d) ?? 0) + 1)
         }
+        for (const [_, x] of cnt) {
+            ans += x * (x - 1)
+        }
     }
-    return ans << 1
+    return ans
 };
