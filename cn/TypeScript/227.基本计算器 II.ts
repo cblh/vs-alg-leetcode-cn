4b8dce196c11cf03548d0251badee6802524b4b9
burlinchen <chenbeiliuhuo@gmail.com>
2024-04-07 09:17:26 +0800

    feat: basic-calculator-ii

--- cn/TypeScript/227.基本计算器 II.ts
+++ cn/TypeScript/227.基本计算器 II.ts
@@ -3,6 +3,8 @@
 // @test("3+2*2")=7
 // @test(" 3/2 ")=1
 // @test(" 3+5 / 2 ")=5
+// @test("0-2147483647")=-2147483647
+// @test("14-3/2")=13
 function calculate(s: string): number {
     const stack: number[] = []
 
@@ -17,12 +19,12 @@ function calculate(s: string): number {
         if (i === n - 1 || char === '+' || char === '-' || char === '*' || char === '/') {
             if (sign === '+') {
                 stack.push(v)
-            } else if (sign === '+') {
+            } else if (sign === '-') {
                 stack.push(-v)
             } else if (sign === '*') {
                 stack.push(stack.pop()! * v)
             } else if (sign === '/') {
-                stack.push(Math.floor(stack.pop()! / v))
+                stack.push(stack[stack.length - 1] > 0 ? Math.floor(stack.pop()! / v) : Math.ceil(stack.pop()! / v))
             }
             sign = char
             v = 0
