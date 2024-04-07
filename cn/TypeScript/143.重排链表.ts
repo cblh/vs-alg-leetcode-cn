b49c8044afd06054246a1ab91c96583b0a3b6e9c
burlinchen <chenbeiliuhuo@gmail.com>
2024-04-07 08:50:30 +0800

    feat: trapping-rain-water-ii

---
+++ cn/TypeScript/143.重排链表.ts
@@ -0,0 +1,22 @@
+// @algorithm @lc id=143 lang=typescript 
+// @title reorder-list
+// @test([1,2,3,4])=[1,4,2,3]
+// @test([1,2,3,4,5])=[1,5,2,4,3]
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify head in-place instead.
+ */
+function reorderList(head: ListNode | null): void {
+    
+};
