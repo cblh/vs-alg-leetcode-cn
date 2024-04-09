bdc2a3ff7a358036e39a7e35b5949f3ea654ef9d
burlinchen <chenbeiliuhuo@gmail.com>
2024-04-09 11:14:32 +0800

    feat: shuffle-an-array

---
+++ cn/TypeScript/384.打乱数组.ts
@@ -0,0 +1,23 @@
+// @algorithm @lc id=384 lang=typescript 
+// @title shuffle-an-array
+class Solution {
+    private nums: number[];
+
+    constructor(nums: number[]) {
+        this.nums = nums;
+    }
+
+    reset(): number[] {
+        return this.nums;
+    }
+
+    shuffle(): number[] {
+        const n = this.nums.length;
+        const res = [...this.nums];
+        for (let i = 0; i < n; i++) {
+            const j = Math.floor(Math.random() * n)
+            ;[res[i], res[j]] = [res[j], res[i]]
+        }
+        return res
+    }
+}
