1a0c16a864078f32dd62b5b47d347a842567822b
burlinchen <chenbeiliuhuo@gmail.com>
2024-04-11 10:50:16 +0800

    feat: remove-k-digits

--- cn/TypeScript/402.移掉 K 位数字.ts
+++ cn/TypeScript/402.移掉 K 位数字.ts
@@ -4,20 +4,16 @@
 // @test("10200",1)="200"
 // @test("10",2)="0"
 function removeKdigits(num: string, k: number): string {
-    const stk: string[] = []
+    const stk: string[] = [];
     for (const c of num) {
         while (k && stk.length > 0 && stk[stk.length - 1] > c) {
-            stk.pop()
-            k--
+            stk.pop();
+            k--;
         }
-        stk.push(c)
+        stk.push(c);
     }
     while (k--) {
-        stk.pop()
+        stk.pop();
     }
-    let i = 0
-    for (; i < stk.length && stk[i] == '0'; i++) {
-    }
-    const ans = stk.slice(i).join('')
-    return ans === '' ? "0" : ans
+    return stk.join('').replace(/^0*/g, '') || '0';
 };
