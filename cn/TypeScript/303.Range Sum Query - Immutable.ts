cf45d2d3171b4dab029c02cd2833410b93c8d510
burlinchen <chenbeiliuhuo@gmail.com>
2025-03-11 14:32:11 +0800

    chore: format

--- cn/TypeScript/303.Range Sum Query - Immutable.ts
+++ cn/TypeScript/303.Range Sum Query - Immutable.ts
@@ -1,114 +1,109 @@
-// @algorithm @lc id=303 lang=typescript 
+// @algorithm @lc id=303 lang=typescript
 // @title range-sum-query-immutable
 interface TreeNodeSegmentTree {
-    l: number
-    r: number
-    v: any
+  l: number;
+  r: number;
+  v: any;
+}
+abstract class SegmentTree<T> {
+  tree: TreeNodeSegmentTree[];
+
+  constructor(
+    protected arr: T[],
+    protected getLeafValue: (v: any) => any,
+    protected getNotLeafValue: (vl: any, vr: any) => any,
+  ) {
+    const n = arr.length;
+    this.tree = Array(4 * n);
+    this._build(0, 0, n - 1);
   }
-  abstract class SegmentTree<T> {
-    tree: TreeNodeSegmentTree[]
-  
-    constructor(
-      protected arr: T[],
-      protected getLeafValue: (v: any) => any,
-      protected getNotLeafValue: (vl: any, vr: any) => any
-    ) {
-      const n = arr.length
-      this.tree = Array(4 * n)
-      this._build(0, 0, n - 1)
-    }
-    protected _build(u: number, l: number, r: number) {
-      if (l === r) {
-        this.tree[u] = {
-          l,
-          r,
-          v: this.getLeafValue(this.arr[l]),
-        }
-        return
-      }
-      let m = Math.floor((l + r) / 2)
-      this._build(2 * u + 1, l, m)
-      this._build(2 * u + 2, m + 1, r)
+  protected _build(u: number, l: number, r: number) {
+    if (l === r) {
       this.tree[u] = {
         l,
         r,
-        v: this.getNotLeafValue(this.tree[2 * u + 1].v, this.tree[2 * u + 2].v),
-      }
+        v: this.getLeafValue(this.arr[l]),
+      };
+      return;
     }
-    protected _update(u: number, pos: number, v: T) {
-      const { l, r } = this.tree[u]
-      if (pos < l || pos > r) {
-        return
-      }
-      if (l === r) {
-        if (l === pos) {
-          this.tree[u].v = this.getLeafValue(v)
-        }
-        return
-      }
-      let m = Math.floor((l + r) / 2)
-      if (pos > m) {
-        this._update(2 * u + 2, pos, v)
-      } else {
-        this._update(2 * u + 1, pos, v)
-      }
-      this.tree[u].v = this.getNotLeafValue(
-        this.tree[2 * u + 1].v,
-        this.tree[2 * u + 2].v
-      )
-    }
-    abstract query(start: number, end: number): any
-    // abstract update(pos: number, value: T): void
+    let m = Math.floor((l + r) / 2);
+    this._build(2 * u + 1, l, m);
+    this._build(2 * u + 2, m + 1, r);
+    this.tree[u] = {
+      l,
+      r,
+      v: this.getNotLeafValue(this.tree[2 * u + 1].v, this.tree[2 * u + 2].v),
+    };
   }
-  
-  /**
-   * Sum segment tree.
-   * Query the sum of given range and update the value in O(Logn) time.
-   * @category segment tree
-   * @example
-   * ```js
-   * const sumArr = new SumArr([2,3,1,7,9])
-   * sumArr.query(0,2)
-   * ```
-   */
-   class SumArr extends SegmentTree<number> {
-    constructor(arr: number[]) {
-      super(
-        arr,
-        x => x,
-        (x, y) => x + y
-      )
+  protected _update(u: number, pos: number, v: T) {
+    const { l, r } = this.tree[u];
+    if (pos < l || pos > r) {
+      return;
     }
-    private _query(u: number, l: number, r: number): number {
-      const { l: _l, r: _r, v } = this.tree[u]
-      if (l <= _l && r >= _r) {
-        return v
+    if (l === r) {
+      if (l === pos) {
+        this.tree[u].v = this.getLeafValue(v);
       }
-      if (r < _l || l > _r) {
-        return 0
-      }
-      let v1 = this._query(2 * u + 1, l, r)
-      let v2 = this._query(2 * u + 2, l, r)
-      return this.getNotLeafValue(v1, v2)
+      return;
     }
-    query(start = 0, end = this.arr.length - 1): number {
-      return this._query(0, start, end)
+    let m = Math.floor((l + r) / 2);
+    if (pos > m) {
+      this._update(2 * u + 2, pos, v);
+    } else {
+      this._update(2 * u + 1, pos, v);
     }
-    update(pos: number, value: number) {
-      this.arr[pos] = value
-      this._update(0, pos, value)
-    }
+    this.tree[u].v = this.getNotLeafValue(this.tree[2 * u + 1].v, this.tree[2 * u + 2].v);
   }
-  
-  
-  class NumArray {
-      segmentTree: SumArr
-      constructor(nums: number[]) {
-          this.segmentTree = new SumArr(nums);
-      }
-  
-      sumRange(left: number, right: number): number {
-          return this.segmentTree.query(left, right)
-      }
+  abstract query(start: number, end: number): any;
+  // abstract update(pos: number, value: T): void
+}
+
+/**
+ * Sum segment tree.
+ * Query the sum of given range and update the value in O(Logn) time.
+ * @category segment tree
+ * @example
+ * ```js
+ * const sumArr = new SumArr([2,3,1,7,9])
+ * sumArr.query(0,2)
+ * ```
+ */
+class SumArr extends SegmentTree<number> {
+  constructor(arr: number[]) {
+    super(
+      arr,
+      (x) => x,
+      (x, y) => x + y,
+    );
   }
-  
+  private _query(u: number, l: number, r: number): number {
+    const { l: _l, r: _r, v } = this.tree[u];
+    if (l <= _l && r >= _r) {
+      return v;
+    }
+    if (r < _l || l > _r) {
+      return 0;
+    }
+    let v1 = this._query(2 * u + 1, l, r);
+    let v2 = this._query(2 * u + 2, l, r);
+    return this.getNotLeafValue(v1, v2);
+  }
+  query(start = 0, end = this.arr.length - 1): number {
+    return this._query(0, start, end);
+  }
+  update(pos: number, value: number) {
+    this.arr[pos] = value;
+    this._update(0, pos, value);
+  }
+}
+
+class NumArray {
+  segmentTree: SumArr;
+  constructor(nums: number[]) {
+    this.segmentTree = new SumArr(nums);
+  }
+
+  sumRange(left: number, right: number): number {
+    return this.segmentTree.query(left, right);
+  }
+}
--- cn/TypeScript/3479.Fruits Into Baskets III.ts
+++ cn/TypeScript/3479.Fruits Into Baskets III.ts
@@ -1,64 +1,64 @@
-// @algorithm @lc id=3791 lang=typescript 
+// @algorithm @lc id=3791 lang=typescript
 // @title fruits-into-baskets-iii
 // @test([4,2,5],[3,5,4])=1
 // @test([3,6,1],[6,4,7])=0
 function numOfUnplacedFruits(fruits: number[], baskets: number[]): number {
-    const valueSegTree = new SegmentTree(baskets)
-    const n = baskets.length;
-    let ans = 0;
-    for (let x of fruits) {
-        if (valueSegTree.findFirstAndUpdate(1,0, n - 1, x) < 0){
-            ans += 1
-        }
+  const valueSegTree = new SegmentTree(baskets);
+  const n = baskets.length;
+  let ans = 0;
+  for (let x of fruits) {
+    if (valueSegTree.findFirstAndUpdate(1, 0, n - 1, x) < 0) {
+      ans += 1;
     }
-    return ans
-};
+  }
+  return ans;
+}
 function createZeroArray(n) {
-    // 计算 (n - 1) 的二进制表示所需的位数
-    const bitLength = (n - 1).toString(2).length;
-    // 计算 2 左移 bitLength 位的结果
-    const length = 2 << bitLength;
-    // 创建一个长度为 length 且元素初始化为 0 的数组
-    const result = new Array(length).fill(0);
-    return result;
+  // 计算 (n - 1) 的二进制表示所需的位数
+  const bitLength = (n - 1).toString(2).length;
+  // 计算 2 左移 bitLength 位的结果
+  const length = 2 << bitLength;
+  // 创建一个长度为 length 且元素初始化为 0 的数组
+  const result = new Array(length).fill(0);
+  return result;
 }
 class SegmentTree {
-    max: number[]
-    constructor(arr: number[]) {
-        const n = arr.length; 
-        this.max = createZeroArray(n)
-        this.build(arr, 1, 0, n - 1)
-    }
+  max: number[];
+  constructor(arr: number[]) {
+    const n = arr.length;
+    this.max = createZeroArray(n);
+    this.build(arr, 1, 0, n - 1);
+  }
 
-    maintain(o: number) {
-        this.max[o] = Math.max(this.max[o * 2], this.max[o * 2 + 1])
-    }
+  maintain(o: number) {
+    this.max[o] = Math.max(this.max[o * 2], this.max[o * 2 + 1]);
+  }
 
-    build(a: number[], o: number, l: number, r: number){
-        if (l === r){
-            this.max[o] = a[l]
-            return
-        }
-        const m = Math.floor((l + r) /2);
-        this.build(a, o * 2, l, m);
-        this.build(a, o* 2 + 1, m + 1, r)
-        this.maintain(o)
+  build(a: number[], o: number, l: number, r: number) {
+    if (l === r) {
+      this.max[o] = a[l];
+      return;
     }
+    const m = Math.floor((l + r) / 2);
+    this.build(a, o * 2, l, m);
+    this.build(a, o * 2 + 1, m + 1, r);
+    this.maintain(o);
+  }
 
-    findFirstAndUpdate(o: number, l: number, r: number, x: number): number{
-        if (this.max[o] < x) {
-            return -1;
-        }
-        if (l === r) {
-            this.max[o] = -1
-            return l;
-        }
-        const m = Math.floor((l + r) / 2);
-        let i = this.findFirstAndUpdate(o * 2, l, m, x);
-        if (i < 0){
-            i = this.findFirstAndUpdate(o * 2 + 1, m + 1, r, x); 
-        }
-        this.maintain(o)
-        return i
+  findFirstAndUpdate(o: number, l: number, r: number, x: number): number {
+    if (this.max[o] < x) {
+      return -1;
     }
+    if (l === r) {
+      this.max[o] = -1;
+      return l;
+    }
+    const m = Math.floor((l + r) / 2);
+    let i = this.findFirstAndUpdate(o * 2, l, m, x);
+    if (i < 0) {
+      i = this.findFirstAndUpdate(o * 2 + 1, m + 1, r, x);
+    }
+    this.maintain(o);
+    return i;
+  }
 }
